f3397308657d81e8de274c5e00a874630da2a8d4
Byrne Reese <byrne@majordojo.com>
2009-06-01 13:09:33 -0700

    made this plugin finally work

--- plugins/LinkChecker/lib/LinkChecker/Plugin.pm
+++ plugins/LinkChecker/lib/LinkChecker/Plugin.pm
@@ -1,72 +1,68 @@
 package LinkChecker::Plugin;
 use strict;
 
-use MT::Plugin;
 use LWP::Simple;
-use URI::Find;
-
-@LinkChecker::Plugin::ISA = qw( MT::Plugin );
-
-sub new {
-    my $self = shift;
-    $self->SUPER::new(@_) or return;
-}
-
-sub init_app {
-    my $plugin = shift;
-    my ($app) = @_; 
-    if ($app->isa('MT::App::CMS')) {
-        $app->add_methods(
-			  link_check => \&do_check,
-			  );
-    }
-}
+use HTML::SimpleLinkExtor;
+use Carp qw( croak );
 
 sub check_links {
-    my $plugin = shift;
-    my ($app) = @_;
-    $app->{cfg}->AltTemplatePath('plugins/LinkChecker/tmpl');
+    my $app = shift;
+    my ($param) = @_;
+    my $q = $app->{query};
+    require MT::Blog;
+    my $blog = MT::Blog->load($q->param('blog_id'));
 
-    my (%param, @message_loop);
+    $param ||= {};
+
+    my (@message_loop);
     my @ids = $app->param('id');
-    my $config = $plugin->get_config_hash;
-    my $text = $config->{text};
     my $blog = $app->blog;
     require MT::Entry;
     foreach my $id (@ids) {
-	my $iter = MT::Entry->load_iter( { id => $id,
-					   blog_id => $blog->id } , undef );
-	my $data = build_link_table($app, 
-				    iter => $iter,
-				    param => \%param 
-				    );
-	
-	use Data::Dumper;
-	print STDERR Dumper(%param);
-	
-	return $app->build_page('linkchecker_check.tmpl',\%param);
+#	my $iter = MT::Entry->load_iter( { id => $id,
+#					   blog_id => $blog->id } , undef );
+#	my $data = build_link_table($app, 
+#				    iter => $iter,
+#				    param => $param 
+#				    );
+#	return $app->load_tmpl( 'link_check.tmpl', $param );
     }
 }
 
 sub do_check {
     my $app = shift;
-    $app->{cfg}->AltTemplatePath('plugins/LinkChecker/tmpl');
-
     my $q = $app->{query};
 
     my $blog = $app->blog;
     require MT::Entry;
-    my $iter = MT::Entry->load_iter( { blog_id => $blog->id } , undef );
+
+    my $iter = MT::Entry->load_iter( { blog_id => $blog->id } );
     my %param;
     my $data = build_link_table($app, 
 				iter => $iter,
 				param => \%param 
 				);
 
-    use Data::Dumper;
-    print STDERR Dumper(%param);
+    return $app->load_tmpl( 'link_check.tmpl', \%param );
+}
 
-    $app->build_page('linkchecker_check.tmpl',\%param);
+sub itemset_handler {
+    my ($app) = @_;
+    $app->validate_magic or return;
+    my $blog = $app->blog;
+
+    require MT::Entry;
+    my @ids = $app->param('id');
+    my $iter = MT::Entry->load_iter( { id => \@ids } );
+
+    my %param;
+    my $data = build_link_table($app, 
+				iter => $iter,
+				param => \%param 
+				);
+    $param{link_data} = $data;
+    $param{ids} = join(',',@ids);
+    return $app->load_tmpl( 'link_check.tmpl', \%param );
 }
 
 sub build_link_table {
@@ -76,27 +72,31 @@ sub build_link_table {
     my $param = $args{param} || {};
 
     my @data;
+
     while (my $e = $iter->()) {
-	print STDERR $e->title() . "\n";
-	my $finder = URI::Find->new(sub {
-	    my($uri, $orig_uri) = @_;
-	    my $row;
-	    $row->{uri}      = $orig_uri;
-	    $row->{title}    = $e->title();
-	    $row->{entry_id} = $e->id();
-	    if ( head $uri ) {
-		print STDERR "$orig_uri is okay\n";
-		$row->{link_status} = "OK";
-		$row->{status} = 1;
-	    } else {
-		print STDERR "$orig_uri cannot be found\n";
-		$row->{link_status} = "ERR";
-		$row->{status} = 0;
+	# TODO - run entry through its text formatter 
+	my $extor = HTML::SimpleLinkExtor->new();
+        $extor->parse( MT->apply_text_filters($e->text, $e->text_filters) );
+        $extor->parse( MT->apply_text_filters($e->text_more, $e->text_filters) );
+
+	foreach my $type (qw(img frame area base href)) {
+	    my @links = $extor->$type();
+	    foreach my $link (@links) {
+		my $row;
+		$row->{type}     = $type;
+		$row->{uri}      = $link;
+		$row->{title}    = $e->title();
+		$row->{entry_id} = $e->id();
+		if ( head $link ) {
+		    $row->{link_status} = "OK";
+		    $row->{status} = 1;
+		} else {
+		    $row->{link_status} = "ERR";
+		    $row->{status} = 0;
+		}
+		push @data, $row;# unless $row->{status};
 	    }
-	    push @data, $row;
-	    return $orig_uri;
-	});
-	$finder->find(\$e->text());
+	}
     }
     return [] unless @data;
 
